78e4b6d66739bfa9b36154930417738cdc446f09
neko967 <113199838+neko967@users.noreply.github.com>
2023-10-10 18:14:50 +0900

    Fix: remove error

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,10 @@ export default async function Cart({
 } : {
   params: { user: string }
 }): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
 
   return (
     <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
+      testtext
     </div>
   );
 }
